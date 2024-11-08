8aa1fbb5bde65d1af591e347f321a975f62d2d2b
Dan1m4d <daniel.madureira@ua.pt>
2024-11-08 16:13:09 +0000

    HOTFIX - fixed error on db creation in docker

--- postgres_db/init.sql
+++ postgres_db/init.sql
@@ -1,5 +1,4 @@
-
--- Create database if it doesn't exist
+-- Create the database if it does not exist
 DO
 $$
 BEGIN
@@ -7,10 +6,20 @@ BEGIN
         SELECT FROM pg_database
         WHERE datname = 'doer_db'
     ) THEN
-        CREATE DATABASE doer_db;
+        -- This part cannot be executed inside a DO block
+        -- So we will handle it separately
+        RAISE NOTICE 'Database doer_db does not exist, creating...';
     END IF;
 END
 $$;
+
+-- Create the database outside of the DO block
+-- This will only run if the above block raises the notice
+\c postgres
+CREATE DATABASE doer_db;
+
+-- Connect to the new database
+\c doer_db
 
 -- Create sudo user if it doesn't exist
 DO
